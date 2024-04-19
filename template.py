44502d4ce26e5d02b23526a9297af31c6b23ca17
Atharva Hudlikar <atharvahudlikar@gmail.com>
2024-04-19 17:07:04 +0800

    svm movie working code added

--- template.py
+++ template.py
@@ -4,6 +4,9 @@ from nltk.stem import WordNetLemmatizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
 import pickle
+import re
+from nltk.corpus import stopwords, wordnet
+import tensorflow as tf
 
 class NLPModel:
     def __init__(self) -> None:
@@ -18,29 +21,76 @@ class NLPModel:
     def display_stats(self) -> None:
         pass
 
-class SVM(NLPModel):
+class SVM_Movie(NLPModel):
 
     nltk.download('wordnet')
     nltk.download('averaged_perceptron_tagger')
 
     def __init__(self) -> None:
         self.lemmatizer = WordNetLemmatizer()
-        self.tv = TfidfVectorizer(stop_words='english')
+        with open('svm_movie_vectorizer.pkl', 'rb') as f:
+            self.tv = pickle.load(f)
         self.model = self.import_weights()
         super().__init__()
     
-    def import_weights(self, dataset):
-        with open(f'svm_{dataset}.pkl', 'rb') as f:
+    def import_weights(self):
+        with open('svm_movie.pkl', 'rb') as f:
             model = pickle.load(f)
         return model
     
     def test_model(self, input):
-        tv_input = np.asarray(self.tv.transform([input]).todense())
-        self.prediction = self.model.predict(tv_input)
+        test_review_tfidf=np.asarray(self.tv.transform([self.preprocess_text(input)]).todense())
+        self.prediction = self.model.predict(test_review_tfidf)[0]
         return self.prediction
     
     def display_stats(self) -> None:
         return {
-            'f1_score': 81.6,
+            'f1_score': 0.808,
+            'accuracy': 0.808,
+            'precision': 0.7658,
+            'recall': 0.9167,
+            'auc': 0.8015536723163841
         }
-        
+    
+    def preprocess_text(self, text):
+        text = re.sub('(<.*?>)', ' ', text)
+        text = re.sub('[,\.!?:()"]', '', text)
+        text = text.strip()
+        text = re.sub('[^a-zA-Z"]',' ', text)
+        text = text.lower()
+        text = self.tagged_lemma(text)
+
+        words = tf.keras.preprocessing.text.text_to_word_sequence(text)
+        stop_words = set(stopwords.words('english'))
+        filtered_words = [w for w in words if not w in stop_words]
+        text = " ".join(filtered_words)
+
+        return text
+    
+    def tagged_lemma(self, string):
+        pos_tagged = nltk.pos_tag(nltk.word_tokenize(string))
+        wordnet_tagged = list(map(lambda x: (x[0], self.pos_tagger(x[1])), pos_tagged))
+        lemmatized_sentence = []
+        for word, tag in wordnet_tagged:
+            if tag is None:
+                lemmatized_sentence.append(word)
+            else:       
+                lemmatized_sentence.append(self.lemmatizer.lemmatize(word, tag))
+        lemmatized_sentence = " ".join(lemmatized_sentence)
+        return lemmatized_sentence
+    
+    def pos_tagger(self, nltk_tag):
+        if nltk_tag.startswith('J'):
+            return wordnet.ADJ
+        elif nltk_tag.startswith('V'):
+            return wordnet.VERB
+        elif nltk_tag.startswith('N'):
+            return wordnet.NOUN
+        elif nltk_tag.startswith('R'):
+            return wordnet.ADV
+        else:         
+            return None
+
+if __name__ == "__main__":
+    svm_movie = SVM_Movie()
+    print(svm_movie.test_model("Olivier Gruner stars as Jacques a foreign exchange college student who takes on and single handedly wipes out a Mexican street gang in this obnoxious and racist film which is so horrible that it's laughable. Bad acting, bad plot and bad fight choreography make Angel Town a Turkey."))
